a7b54d82dcb2bfcfe76f3e513d321a432825a869
Ben Albrecht <workbench@workbench>
2020-11-26 23:57:46 -0600

    tidy

--- emacs.d/config/init.el
+++ emacs.d/config/init.el
@@ -1,18 +1,6 @@
 ;; Loads the config files...
 
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-
-
+(require 'package)
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
@@ -40,7 +28,6 @@
        ((and (eq isdir nil) (string= (substring path -3) ".el"))
         (load (file-name-sans-extension fullpath)))))))
 
-
 (load-directory "~/.emacs.d/config")
 
 (custom-set-variables
@@ -49,13 +36,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(menu-bar-mode nil)
- '(package-selected-packages (quote (face-explorer gnu-elpa-keyring-update ##)))
+ '(package-selected-packages
+   (quote
+    (powershell face-explorer gnu-elpa-keyring-update ##)))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nul)
  '(tooltip-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+  ;;If you edit it by hand, you could mess it up, so be careful.
+  ;;Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right. 
  '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :height 98 :width normal)))))
+;; (set-frame-font "Monaco:size=12")
+ (setq default-frame-alist nil)
+ 
--- emacs.d/config/steeple.el
+++ emacs.d/config/steeple.el
@@ -1,19 +1,16 @@
-
 ;; Steeple: the VampMacs Package Management System 
 
-
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages"))
-(add-to-list 'package-archives '("marmalade". "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t )
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t )
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t )			
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages") t )
+(add-to-list 'package-archives '("marmalade". "http://marmalade-repo.org/packages/") t )
 
 ;; setup auto package install
-(package-initialize)
+
 (unless package-archive-contents
   (package-refresh-contents))
 (package-install-selected-packages)
 
 ;; fix some issues with elpa
-(setq package-check-signature t)
-
-
+(setq package-check-signature nil)
